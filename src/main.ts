20c6a40ed44a482bb670ece9b200c525f5e0b64a
Bruno Noriller <noriller@bb.com.br>
2021-01-02 10:57:56 -0300

    stable version with env file

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,11 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-const port = 3333;
+const port = process.env.PROCESS_PORT || 3333;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
-  Logger.log(`🚀 Server running on http://localhost:${port}`, 'Bootstrap');
+  Logger.log( `🚀 Server running on http://localhost:${ port }`, 'Bootstrap' );
 }
 bootstrap();
